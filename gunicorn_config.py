73c1b3e9e6537376690c258ac3854539b4ba00eb
Hyperiongate <jim@shift-work.com>
2025-07-15 11:42:45 -0700

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,7 +9,7 @@ backlog = 2048
 workers = int(os.environ.get("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2 + 1))
 worker_class = "sync"
 worker_connections = 1000
-timeout = 30
+timeout = 120
 keepalive = 2
 
 # Restart workers after this many requests to prevent memory leaks
